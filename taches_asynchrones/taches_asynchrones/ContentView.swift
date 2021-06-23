ea76ab9a0a1b86871d3bb358e0d44059089d7554
Maxime Britto <maxime@purplegiraffe.fr>
2021-06-23 17:58:32 +0200

    21_6_Lancer_plusieurs_tâches_en_parallele_avec_async_let

--- taches_asynchrones/taches_asynchrones/ContentView.swift
+++ taches_asynchrones/taches_asynchrones/ContentView.swift
@@ -9,19 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     @State var loadedText = "Prêt"
-    @State var enteredUrl = "https://www.purplegiraffe.fr"
+    @State var enteredSiteUrl = "https://www.purplegiraffe.fr"
+    @State var enteredImageUrl = "https://cdn.fs.teachablecdn.com/BvG1QkLgRvqB6kia04fp"
+    @State var isLoading = false
+    @State var loadedImage:UIImage?
     var body: some View {
         VStack {
-            TextField("Adresse du site", text: $enteredUrl)
+            TextField("Adresse du site", text: $enteredSiteUrl)
                 .textFieldStyle(.roundedBorder)
                 .textContentType(UITextContentType.URL)
                 .disableAutocorrection(true)
                 .autocapitalization(UITextAutocapitalizationType.none)
+            TextField("Adresse du site", text: $enteredImageUrl)
+                .textFieldStyle(.roundedBorder)
+                .textContentType(UITextContentType.URL)
+                .disableAutocorrection(true)
+                .autocapitalization(UITextAutocapitalizationType.none)
+            if isLoading {
+                ProgressView()
+            } else {
+                Button {
+                    isLoading = true
+                    async {
+                        (loadedText, loadedImage) = await loadDataFromTheInternet()
+                        isLoading = false
+                    }
+                } label: {
+                    Label("Charger les données", systemImage: "arrow.triangle.2.circlepath.circle")
+                }
                 
-            Button {
-                loadedText = loadTextFromTheInternet()
-            } label: {
-                Label("Charger les données", systemImage: "arrow.triangle.2.circlepath.circle")
+            }
+            if let image = loadedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: ContentMode.fit)
+                
             }
             Text(loadedText)
             Spacer()
@@ -29,16 +51,19 @@ struct ContentView: View {
         .padding()
     }
     
-    func loadTextFromTheInternet() -> String {
-        guard let url = URL(string: enteredUrl) else { return "URL incorrecte" }
-        let htmlContent:String?
+    func loadDataFromTheInternet() async -> (String, UIImage?) {
+        guard let siteUrl = URL(string: enteredSiteUrl),
+              let imageUrl = URL(string: enteredImageUrl) else { return ("URL incorrecte", nil) }
+        var htmlContent:String? = nil
+        var imageContent:UIImage? = nil
         do {
-            let data = try Data(contentsOf: url)
-            htmlContent = String(data: data, encoding: String.Encoding.utf8)
+            async let (siteData, _) = URLSession.shared.data(from: siteUrl, delegate: nil)
+            async let (imageData, _) = URLSession.shared.data(from: imageUrl, delegate: nil)
+            htmlContent = try await String(data: siteData, encoding: String.Encoding.utf8)
+            imageContent = try await UIImage(data: imageData)
         } catch {
-            htmlContent = nil
         }
-        return htmlContent ?? "Aucune donnée disponible pour cette adresse"
+        return (htmlContent ?? "Aucune donnée disponible pour cette adresse", imageContent)
     }
 }
 
