10c4767b4dd1aafe3c6e38231ff25568f975b897
Maxime Britto <maxime@purplegiraffe.fr>
2021-07-05 11:14:38 +0200

    32_4_Confirmer_une_action_dangereuse

--- todos/todos/TodoListView.swift
+++ todos/todos/TodoListView.swift
@@ -10,52 +10,94 @@ import SwiftUI
 struct TodoListView: View {
     @State var todoList:[Todo] = []
     @State var searchFilter = ""
+    @State var fastMode = false
+    @State var newTaskName = ""
+    @State var wantsToEmptyList = false
     var body: some View {
         NavigationView {
-            List {
-                ForEach($todoList) { $todo in
-                    if searchFilter.isEmpty || todo.title.lowercased().contains(searchFilter.lowercased()) {
-                        TodoRow(todo: todo).onTapGesture {
-                            todo.completed.toggle()
-                        }.swipeActions(allowsFullSwipe:false) {
-                            
-                            Button(role: .destructive) {
-                                if let index = todoList.firstIndex(of: todo) {
-                                    withAnimation {
-                                        _ = todoList.remove(at: index)
+            VStack {
+                HStack {
+                    Toggle(isOn: $fastMode) {
+                        Label("Fast", systemImage: "hare")
+                    }
+                    .padding()
+                    .toggleStyle(.button)
+                    Button(role: .destructive) {
+                        wantsToEmptyList = true
+                    } label: {
+                        Label("Vider la liste", systemImage: "trash")
+                    }.confirmationDialog("Vider la liste", isPresented: $wantsToEmptyList) {
+                        Button(role: .destructive) {
+                            todoList.removeAll()
+                        } label: {
+                            Label("Effacer toutes les tâches", systemImage: "trash")
+                        }
+                    } message: {
+                        Text("Vider cette liste va effacer toutes vos tâches définitivement. Êtes vous certain ?")
+                    }
+
+
+
+                }
+                if fastMode {
+                    TextField("Nouvelle tâche", text: $newTaskName).padding()
+                }
+                List {
+                    ForEach($todoList) { $todo in
+                        if searchFilter.isEmpty || todo.title.lowercased().contains(searchFilter.lowercased()) {
+                            TodoRow(todo: todo).onTapGesture {
+                                todo.completed.toggle()
+                            }.swipeActions(allowsFullSwipe:false) {
+                                
+                                Button(role: .destructive) {
+                                    if let index = todoList.firstIndex(of: todo) {
+                                        withAnimation {
+                                            _ = todoList.remove(at: index)
+                                        }
                                     }
+                                } label: {
+                                    Label("Supprimer", systemImage: "trash")
                                 }
-                            } label: {
-                                Label("Supprimer", systemImage: "trash")
-                            }
-                        }.swipeActions(edge: .leading) {
-                            Button {
-                                todo.isImportant.toggle()
-                            } label: {
-                                Label("Important", systemImage: "star")
-                            }
-                        }.listRowSeparator(.hidden)
+                            }.swipeActions(edge: .leading) {
+                                Button {
+                                    todo.isImportant.toggle()
+                                } label: {
+                                    Label("Important", systemImage: "star")
+                                }
+                            }.listRowSeparator(.hidden)
+                        }
                     }
                 }
-            }
-            .navigationTitle("Todos")
-            .task {
-                await loadTodoList()
-            }
-            .refreshable {
-                await loadTodoList()
-            }
-            .searchable(text: $searchFilter) {
-                switch searchFilter.count {
-                case 0:
-                    Text("🚀").searchCompletion("fusée")
-                    Text("☀️").searchCompletion("soleil")
-                case 1:
-                    Text("🏝").searchCompletion("île")
-                    Text("🐴").searchCompletion("porro")
-                default:
-                    Text("✅").searchCompletion("ok")
+                .navigationBarItems(trailing:
+                    Button(action: {
+                        
+                    }, label: {
+                        Label("Ajouter", systemImage: "plus")
+                    }).buttonStyle(.bordered)
+                                        .controlProminence(.increased)
+                                        .controlSize(.large)
+                )
+                .navigationTitle("Todos")
+                .task {
+                    await loadTodoList()
                 }
+                .refreshable {
+                    await loadTodoList()
+                }
+                .searchable(text: $searchFilter) {
+                    switch searchFilter.count {
+                    case 0:
+                        Text("🚀").searchCompletion("fusée")
+                        Text("☀️").searchCompletion("soleil")
+                    case 1:
+                        Text("🏝").searchCompletion("île")
+                        Text("🐴").searchCompletion("porro")
+                    default:
+                        Text("✅").searchCompletion("ok")
+                    }
+                }.onSubmit(of: .search) {
+                    
+            }
             }
         }
         
